a6a67bf1fdccd9054f6b60716a2f561f5ccae1ac
syacooms <syacooms@gmail.com>
2020-08-12 16:21:42 +0900

    [add] review.16:00

--- Hoboard/WebContent/review.jsp
+++ Hoboard/WebContent/review.jsp
@@ -1,35 +1,30 @@
-<%@ page language="java" contentType="text/html; charset=UTF-8"
-	pageEncoding="UTF-8"%>
-
+<%@page import="Reserve.Reserve_Dto"%>
+<%@page import="member.Member_Dto"%>
 <%@page import="review.Review_Dto"%>
 <%@page import="java.util.List"%>
 
 <%@ include file="module/header.jsp"%>
-<%-- //���Ƿα���
-<%
+
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+
+
+
+<%-- <%
 Object ologin = session.getAttribute("login");
-MemberDto mem = null;
+Member_Dto mem = null;
 if(ologin == null){
 	%>
 	<script type="text/javascript">
-	alert("�α��� �� �ֽʽÿ�");
+	alert("로그인 할 수 없습니다.");
 	location.href = "login.jsp";
-	</script>	
+	</script>
 	<%
 }
+mem = (Member_Dto)ologin;
 
-mem = (MemberDto)ologin;
 %> --%>
-
-
-<%
-List<Review_Dto> list = (List<Review_Dto>)request.getAttribute("reviewlist");
-
-System.out.println(list);
-
-%>
-
-
 
 <%-- //�˻�
 <%
@@ -51,44 +46,44 @@ if(searchWord == null){
 
 %> --%>
 
-<input type="text" name="text"><button>�˻�</button> 
+
+
+<%
+List<Review_Dto> list = (List<Review_Dto>)request.getAttribute("reviewlist");
+%>
+
+<input type="text" name="text"><button>검색</button> 
 &nbsp;&nbsp;&nbsp;
 <select id="sel">
-	<option value="1" selected="selected">����</option>
-	<option value="2">����</option>
-	<option value="3">�����̸�</option>
-	<option value="4">�ۼ���</option>
-	<option value="5">����</option>	
-	<option value="6">����</option>
+	<option value="1" selected="selected">선택</option>
+	<option value="2">작성자</option>
+	<option value="3">병원이름</option>
+	<option value="4">제목</option>
+	<option value="5">내용</option>	
+	<option value="6">평점</option>
 </select>
 <br><br>
 
-����Ʈ Ȯ��
-<table border="1">
-<%-- <%
-for(int i=0; i<5; i++){
-%>
-<table border="1">
-<col width="70"><col width="70"><col width="70">
+
+
+<table style="width: 700">
+<col width="100"><col width="300"><col width="300">
 <tr>
-	<th>[=busi_name%>�����̸�]</th><th>TITLE</th><td><a href="review_detail.jsp">�������ѳ�����</a></td>
+	<td height="2" bgcolor="#0000ff" colspan="3"></td>
 </tr>
-<tr>	
-	<!-- INDVD_NAME --> <th>name</th><td>ȫ��..</td>
-	<!-- W_DATE     --> <td>17:00</td>
-	<!-- VIEW COUNT --> <td>5ȸ</td>
-	<!-- SCORE      --> <td>4.5����</td>
-</tr>	
+<tr>
 
+	<th>ID</th>
+	<th>CONTENT</th>
+</tr>
+<tr>
+	<td height="2" bgcolor="#0000ff" colspan="3"></td>
+</tr>
 <%
-}
-%> --%>
-
-<%-- <%
-if(list.size() == 0){
+if(list == null || list.size() == 0){
 	%>
 	<tr>
-		<td colspan = "3" align="center">�ۼ��� �ıⰡ �������� �ʽ��ϴ�.</td>
+		<td colspan = "3" align="center">작성된 후기가 없습니다.</td>
 	</tr>
 <%
 }else{
@@ -97,30 +92,41 @@ if(list.size() == 0){
 	%>
 
 <tr>
-	<th><%=rev.getBusi_id()%></th><th><%=rev.getTitle()%></th><td><a href="review_detail.jsp"><%=rev.getContent()%></a></td>
+	<th>[병원이름]</th><th><%=rev.getTitle()%></th><td><a href="review_detail.jsp&=<%=rev.getReview_seq()%>"><%=rev.getContent()%></a></td>
 </tr>
 <tr>	
-	<!-- INDVD_NAME --> <th><%=rev.getIndvd_id()%></th>
-	<!-- W_DATE     --> <td><%=rev.getW_date() %></td>
-	<!-- VIEW COUNT --> <td><%=rev.getViewcount() %></td>
-	<!-- SCORE      --> <td><%=rev.getScore() %></td>
+	<!-- INDVD_NAME --> <th>작성자</th><td><%=rev.getIndvd_id()%></td>
+	<!-- W_DATE     --> <th>작성일</th><td><%=rev.getW_date() %></td>
+	<!-- VIEW COUNT --> <th>조회수</th><td><%=rev.getViewcount() %></td>
+	<!-- SCORE      --> <th>평점</th><td><%=rev.getScore() %></td>
 </tr>
-
+<tr>
+	<td height="2" bgcolor="#000fff" colspan="3"></td>
+</tr>
 	<%
 	}
 }
-%> --%>
-
+%>
 </table>
+<br>
+<br>
 
-<!-- TODO -->
-<button type="button" onclick="writeBbs(<%-- <%=dao.getSeq() %> --%>)">�۾���</button>
+<% //TODO after scriptlet session ID add change %>
+<button type="button" id="getId" onclick="writeBtn()" value="<%="fff"%>">글쓰기</button>
+
 
 <script type="text/javascript">
-window.onload = function() {
-	location.href = "REVIEW";
-}
+function writeBtn(){
+	
+	
+	let ID = document.getElementById("getId").value;
+	
+	//TODO dummy
+	let BUSI_ID = "hanbang";
+	let seq = "7"; 
 
+	location.href="REVIEW?review=writeView&id=" + ID + "&busi=" + BUSI_ID + "&seq=" + seq;
+}
 </script>
 
 <%@ include file="module/footer.jsp"%>
--- Hoboard/src/review/ReviewController.java
+++ Hoboard/src/review/ReviewController.java
@@ -1,80 +1,222 @@
 package review;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.websocket.SendResult;
+
+import Reserve.Reserve_Dto;
+import Util.UtilEx;
+import member.Member_Dto;
 
 @WebServlet("/REVIEW")
 public class ReviewController extends HttpServlet {
+	
+	ServletConfig mConfig = null;
+	final int BUFFER_SIZE = 8192;
+	
+	//TODO file download
+	/*
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		
+		super.init(config);
+		mConfig = config;		// file upload load
+	}*/
 
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 
 		String review = req.getParameter("review");
+		Review_Dao dao = Review_Dao.getInstance(); 
 		
-
-		if(review.equals("list")) {
-			Review_Dao dao = Review_Dao.getInstance(); 
+		//review_Main
+		if(review.equals("reviewlist")) {
+			System.out.println("---review list---");
+			
 			List<Review_Dto> list = dao.getReviewList();
-
-
-			req.setAttribute("reviewlist", list); 
-			forward("review.jsp", req, resp);
+			
+			req.setAttribute("reviewlist", list);
+			UtilEx.forward("review.jsp", req, resp);
 		
-	   
+		
+		//review_WriteView
 		}else if(review.equals("writeView")) {
-			resp.sendRedirect("review_write.jsp");
-		
+			System.out.println("---review writeView ---");
+			
+			
+			//dummy RESERVE DATA -> REVIEW 
+			//getSession ID
+			String id = req.getParameter("id");
+			//getBusi_ID
+			String busi = req.getParameter("busi");
+			//getSeq
+			int seq = Integer.parseInt(req.getParameter("seq"));
+			
+			//check
+			System.out.println("id check :" + id);
+			System.out.println("busi check :" + busi);
+			System.out.println("seq check :" + seq);
+			
+			//status lookup method
+			String check = dao.checkStatus(id ,seq);
+			
+			//status check
+			System.out.println(check);
+			
+			
+			//reserve MEMBER - NAME
+			Member_Dto N_list = dao.getBusi_Name(busi, seq);
+			
+			//reserve MEMBER - NAME check
+			System.out.println(N_list.toString());
+			
+			//reserve CATE
+			Reserve_Dto C_list = dao.getReserve_Cate(id, seq);
+			
+			//reserve CATE check
+			System.out.println(C_list.toString());
+			
+			//reserve all data Transfer
+			Reserve_Dto R_list = dao.getReserve_Data(id, seq);
+			
+			//reserve list check
+			System.out.println(R_list.toString());
+			
+
+			
+			if(check.equals("3")) {
+				
+				
+				req.setAttribute("businame", N_list);
+				req.setAttribute("reservecate", C_list);
+				req.setAttribute("reservelist", R_list);
+				
+				UtilEx.forward("review_write.jsp", req, resp);
+				
+			}else {
+				resp.sendRedirect("review.jsp");
+			}
 	    }
 		else if(review.equals("업데이트")) {
-			
 		}
-	
 	}
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		/*
-		 * req.setCharacterEncoding("utf-8");
-		 * 
-		 * String review = req.getParameter("review"); if(review.equals("writedata")) {
-		 * 
-		 * String BUSI_id = req.getParameter("BUSI_id"); String INDVD_id =
-		 * req.getParameter("INDVD_id"); String title = req.getParameter("title");
-		 * String content = req.getParameter("content"); int score =
-		 * Integer.parseInt(req.getParameter("score"));
-		 * 
-		 * System.out.println("BUSI_id:" + BUSI_id); System.out.println("INDVD_id:" +
-		 * INDVD_id); System.out.println("title:" + title); System.out.println("content"
-		 * + content); System.out.println("score:" + score);
-		 * 
-		 * // DB insert Review_Dao dao = Review_Dao.getInstance(); dao.writeReview(new
-		 * Review_Dto(BUSI_id, INDVD_id, title, content, score));
-		 * 
-		 * resp.sendRedirect("list");
-		 * 
-		 * List<Review_Dto> list = dao.getReviewList();
-		 * 
-		 * req.setAttribute("reviewlist", list); forward("review.jsp", req, resp);
-		 * 
-		 * 
-		 * //resp.sendRedirect("review.jsp");
-		 */			
-			
-		/*
-		 * }
-		 */
-	}
-
-	private void forward(String link, HttpServletRequest req, HttpServletResponse resp)throws ServletException, IOException {
-		RequestDispatcher dis = req.getRequestDispatcher(link);
-		dis.forward(req, resp);		
+		req.setCharacterEncoding("UTF-8");
 		
+		String review = req.getParameter("review");
+		
+		
+		Review_Dao dao = Review_Dao.getInstance();
+		
+
+	
+		
+		if(review.equals("writecomplete")) {
+			
+			String indvd_id = req.getParameter("indvd_id");
+			String busi_id = req.getParameter("busi_id");
+			//TODO LIST 병원 이름
+			String cate = req.getParameter("cate");
+			
+			String id = req.getParameter("id");
+			String title = req.getParameter("title");
+			String content = req.getParameter("content");
+			String score = req.getParameter("score");
+			String fileupload = req.getParameter("fileupload");
+			
+			//Parameter check
+			System.out.println("indvd_id: " + indvd_id);
+			System.out.println("busi_id: " + busi_id);
+			System.out.println("cate: " + cate);
+			System.out.println("id: " + id);
+			System.out.println("title: " + title);
+			System.out.println("content: " + content);
+			System.out.println("score: " + content);
+			System.out.println("fileupload: " + fileupload);
+			
+			
+			
+			//dao.processUploadFile(fileItem, dir);
+			
+			
+			
+			
+			
+			//TODO FileDownload
+			/*
+			String filename = req.getParameter("filename");
+			String sseq = req.getParameter("seq");
+			int seq = Integer.parseInt(sseq);
+			System.out.println("seq:" + seq);
+			
+			// download 횟수 증가(DB)
+			Review_Dao dao = Review_Dao.getInstance();
+			dao.downcount(seq);
+			
+			//System.out.println(filename);
+			//System.out.println(seq);
+			BufferedOutputStream out = new BufferedOutputStream(resp.getOutputStream());
+			
+			
+			
+			
+			String filePath = "";
+			
+			// tomcat(server)
+			filePath = mConfig.getServletContext().getRealPath("/upload");
+			
+			
+			// 폴더(client)
+			// filePath = "d:\\tmp";
+			
+			filePath = filePath + "\\" + filename;
+			System.out.println("filePath: " + filePath);
+			
+			File f = new File(filePath);
+			
+			if( f.exists() && f.canRead() ) {
+				
+				// download window set
+				resp.setHeader("Content-Disposition", "attachment; filename=\"" + filename + "\";");
+				resp.setHeader("Content-Transfer-Encoding", "binary;");
+				resp.setHeader("Content-Length", "" + f.length());
+				resp.setHeader("Pragma", "no-cache;"); 
+				resp.setHeader("Expires", "-1;");
+				
+				// 파일 생성, 기입
+				BufferedInputStream fileInput = new BufferedInputStream(new FileInputStream(f));
+				
+				byte buffer[] = new byte[BUFFER_SIZE];
+				
+				int read = 0;
+				
+				while((read = fileInput.read(buffer)) != -1) {
+					out.write(buffer, 0, read);		// 실제 다운로드
+				}
+				//꼭 기입할 것
+				fileInput.close();
+				out.flush();
+			
+		//getParameter session ID
+		String id = req.getParameter("id");
+		
+		*/
+		
+		}
+	
 	}
 }
